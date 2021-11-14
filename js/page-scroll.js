f6343a46698c9c01ef7bf34cc00cd8cb17a7f9ec
Kusuma Hendra <k.kusumahendra@gmail.com>
2021-11-14 12:04:54 +0800

    feat: fade animation with scrollmagic up to story 2

--- js/page-scroll.js
+++ js/page-scroll.js
@@ -2,32 +2,79 @@ $(window).on('load', function () {
     var ctrl = new ScrollMagic.Controller({
         globalSceneOptions: {},
     });
-    var fadein_tween = TweenMax.fromTo('.section-bg', 1,
-        { 
-            yPercent: 100, opacity: 0.2 
-        }, 
-        { 
-            yPercent: 0, opacity: 1, ease: Power1.easeInOut 
-        }
-    );
-
-    // $('.section').each(function (index, element) {
-    //     // set pin
-    //     new ScrollMagic.Scene({
-    //         triggerHook: 'onLeave',
-    //         triggerElement: this,
-    //     })
-    //         .setPin(this)
-    //         .addTo(ctrl);
-    // }); //hero-story each
 
     $('.section').each(function (index, element) {
+        var sectionBg = $(this).find('.section-bg.fixed');
+        var sectionOverlay = $(this).find('.bg-overlay.fixed');
+
+        var tl = new TimelineMax()
+            .add(
+                TweenMax.from(sectionBg, 0.5, {
+                    opacity: 0,
+                    // visibility: 'hidden',
+                })
+            )
+            .add(
+                TweenMax.from(sectionOverlay, 0.5, {
+                    opacity: 0,
+                    // visibility: 'hidden',
+                }),
+                0
+            );
+
+        new ScrollMagic.Scene({
+            triggerElement: this,
+            offset: -1,
+        })
+            .setTween(tl)
+            .addTo(ctrl);
+
+        new ScrollMagic.Scene({
+            triggerElement: this,
+            duration: '200%',
+        })
+            .on('enter', function () {
+                $(element).addClass('active');
+            })
+            .on('leave', function () {
+                $(element).removeClass('active');
+            })
+            .addTo(ctrl);
+
+        new ScrollMagic.Scene({
+            triggerElement: this,
+            duration: '100%',
+            offset: 20
+        })
+            .addIndicators({
+                name: 'Timeline',
+                colorTrigger: 'gray',
+                colorStart: 'gray',
+                colorEnd: 'gray',
+            })
+            .on('enter', function () {
+                video = $(element).find('video').get(0);
+                if (video) {
+                    video.play();
+                }
+            })
+            .on('leave', function () {
+                video = $(element).find('video').get(0);
+                if (video) {
+                    console.log('pausing video');
+                    video.pause();
+                    video.currentTime = 0;
+                }
+            })
+            // .setTween(tl)
+            .addTo(ctrl);
+
         new ScrollMagic.Scene({
             triggerHook: 'onLeave',
             triggerElement: this,
             offset: -1,
         })
-            // .setTween(fadein_tween)
+            .addTo(ctrl) // scene end
             .on('leave', function (event) {
                 if (event.scrollDirection === 'REVERSE') {
                     TweenLite.to(window, 1, {
@@ -38,34 +85,25 @@ $(window).on('load', function () {
                         ease: Power4.easeOut,
                     });
                 }
-            })
-            .addTo(ctrl); // scene end
+            });
 
         new ScrollMagic.Scene({
             triggerElement: this,
             triggerHook: 'onEnter',
             offset: 1,
         })
-            .setTween(fadein_tween)
+            // .setTween(tl)
             .addTo(ctrl)
             .on('enter', function (event) {
-                console.log('triggered');
                 if (event.scrollDirection === 'FORWARD') {
-                    console.log(event.scrollDirection);
-                    // console.log(element);
+                    // $(element).addClass('active');
                     TweenLite.to(window, 1, {
                         scrollTo: {
-                            // y: '.section' + (index + 1),
                             y: $(element).offset().top,
-                            // y: $(window).height() + (index + 1),
                             autoKill: false,
                         },
                         ease: Power4.easeOut,
                     });
-
-                    // console.log(event.scrollDirection);
-                    // TweenLite.to(window, 1, {scrollTo:{y:".hero" + (index+1),
-                    //     autoKill:false},ease: Power4.easeOut});
                 } // scene end
             });
     }); //hero each
